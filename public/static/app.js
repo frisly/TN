f81972df568a21ecc7c6c17d4e37d8ea61ebae25
Frisly Soberanis <frisly@playfrisly.com>
2020-04-22 18:06:25 -0400

    addedjson googlesheet parse and display

--- public/static/app.js
+++ public/static/app.js
@@ -1,27 +1,42 @@
-const events = {
-  title: 'Hello',
-  subline: 'w/ Lupe Maravillosa',
-  link: 'https://www.youtube.com'
+const url = 'https://spreadsheets.google.com/feeds/cells/1-SPWaneXIZ46CgGp8xcTTlgHcs1jNgEv-K1tz0-gqsY/1/public/full?alt=json'
+
+function Get(url){
+  var Httpreq = new XMLHttpRequest(); // a new request
+  Httpreq.open("GET",url,false);
+  Httpreq.send(null);
+  return Httpreq.responseText;          
 }
 
-console.log(events.link);
+var sheet = JSON.parse(Get(url))
+
+var event1title = sheet.feed.entry[4].content.$t
+var event1link = sheet.feed.entry[5].content.$t
+var event2title = sheet.feed.entry[7].content.$t
+var event2link = sheet.feed.entry[8].content.$t
+var event3title = sheet.feed.entry[10].content.$t
+var event3link = sheet.feed.entry[11].content.$t
+
+
+console.log(sheet)
 
 
 let val = `
-<a href="${events.link}"><div id="event1" class=" border-solid border-2 border-red-200 m-2 h-16">
-<h1 class="text-white mt-1 ml-4 font-bold">${events.title}</h1>
+<a href="${event1link}">
+    <div id="event1" class=" flex border-solid border-2 border-red-200 m-2 h-16 justify-center">
+      <h1 class="text-white mt-1 ml-4 font-bold self-center">${event1title}</h1>
+    </div>
+</a>
 
-</div></a>
-<a href="${events.link}">
-<div id="event2" class="border-solid border-2 border-yellow-200 m-2 h-16">
-<h1 class="text-white mt-1 ml-4 font-bold">${events.title}</h1>
+<a href="${event2link}">
+<div id="event2" class="border-solid flex border-2 border-yellow-200 m-2 h-16 justify-center">
+<h1 class="text-white mt-1 ml-4 font-bold self-center">${event2title}</h1>
 </div>
 </a>
-<a href="${events.link}">
-<div id="event2" class="border-solid border-2 border-blue-200 m-2 h-16"></div>
+
+<a href="${event3link}">
+<div id="event2" class="flex border-solid border-2 border-blue-200 m-2 h-16 justify-center">
+<h1 class="text-white mt-1 ml-4 font-bold self-center">${event3title}</h1></div>
 </a>
 
 `
-
-
 document.getElementById('upcomingevents').innerHTML = val;
