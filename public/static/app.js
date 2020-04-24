c8c51c3c6fa98a52ac02a7c9ecd8250d872d950e
Frisly Soberanis <frisly@playfrisly.com>
2020-04-23 20:15:26 -0400

    pop menu

--- public/static/app.js
+++ public/static/app.js
@@ -23,20 +23,30 @@ console.log(sheet)
 let val = `
 <a href="${event1link}">
     <div id="event1" class=" flex border-solid border-2 border-red-200 m-2 h-16 ">
-      <h1 class="text-white mt-1 ml-4 font-bold self-center">${event1title}</h1>
+      <h1 class="text-white mt-1 ml-4 font-bold self-center lg:text-xl">${event1title}</h1>
     </div>
 </a>
-
 <a href="${event2link}">
 <div id="event2" class="border-solid flex border-2 border-yellow-200 m-2 h-16">
-<h1 class="text-white mt-1 ml-4 font-bold self-center">${event2title}</h1>
+<h1 class="text-white mt-1 ml-4 font-bold self-center lg:text-xl">${event2title}</h1>
 </div>
 </a>
 
 <a href="${event3link}">
 <div id="event2" class="flex border-solid border-2 border-blue-200 m-2 h-16">
-<h1 class="text-white mt-1 ml-4 font-bold self-center">${event3title}</h1></div>
+<h1 class="text-white mt-1 ml-4 font-bold self-center lg:text-xl">${event3title}</h1></div>
 </a>
 
 `
 document.getElementById('upcomingevents').innerHTML = val;
+
+
+
+function menu() { 
+  var element = document.getElementById("hiddenMenuDiv");
+  if (element.style.display === "block") {
+      element.style.display = "none";
+  } else {
+      element.style.display = "block";
+  }
+}
